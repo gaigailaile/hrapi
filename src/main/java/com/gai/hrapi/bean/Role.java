527045528f926b3639dec436ce8e5a22009bea1a
aiyanghui521 <932820942@qq.com>
2019-11-05 17:13:19 +0800

    hr项目后台

---
+++ src/main/java/com/gai/hrapi/bean/Role.java
@@ -0,0 +1,13 @@
+package com.gai.hrapi.bean;
+
+import lombok.Data;
+
+/**
+ * Created by gaigaicoming on 2019/11/5.
+ */
+@Data
+public class Role {
+    private Long id;
+    private String name;
+    private String nameZh;
+}
