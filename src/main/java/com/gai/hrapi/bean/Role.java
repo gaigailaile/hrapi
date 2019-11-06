a931c2e2c91356b0b4caa9675a74bcdeb0c9da91
aiyanghui521 <932820942@qq.com>
2019-11-06 16:14:43 +0800

    完成登录验证

--- src/main/java/com/gai/hrapi/bean/Role.java
+++ src/main/java/com/gai/hrapi/bean/Role.java
@@ -2,11 +2,13 @@ package com.gai.hrapi.bean;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * Created by gaigaicoming on 2019/11/5.
  */
 @Data
-public class Role {
+public class Role implements Serializable{
     private Long id;
     private String name;
     private String nameZh;
